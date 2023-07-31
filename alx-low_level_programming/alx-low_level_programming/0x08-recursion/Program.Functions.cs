4252e7bd0942dcc4bff14adb23ea3d3dc7824428
Ifiok Ekott <ekottifiok@gmail.com>
2023-07-31 18:13:06 +0100

    Initial commit

---
+++ alx-low_level_programming/alx-low_level_programming/0x08-recursion/Program.Functions.cs
@@ -0,0 +1,78 @@
+﻿partial class Program
+{
+    static void _puts_recursion(string s, int iter = 0)
+    {
+        if (iter == s.Length)
+        {
+            Write('\n');
+            return;
+        }
+        Write(s[iter]);
+        _puts_recursion(s, iter + 1);
+    }
+
+    static void _print_rev_recursion(string s, int iter = 0)
+    {
+        if (iter == s.Length)
+        {
+            Write('\n');
+            return;
+        }
+        Write(s[s.Length-iter-1]);
+        _print_rev_recursion(s, iter + 1);
+    }
+
+    static int _strlen_recursion(string s, int iter = 0)
+    {
+        if (iter == s.Length)
+            return (0);
+
+        return 1 + _strlen_recursion(s, iter + 1);
+    }
+
+    static int factorial(int d)
+    {
+        if (d < 0)
+        {
+            return -1;
+        }
+        else if (d == 0)
+        {
+            return 1;
+        }
+        checked  // for overflow
+        {
+            return d * factorial(d - 1);
+        }
+    }
+
+    static int _pow_recursion(int x, int y)
+    {
+        if (y < 0)
+        {
+            return -1;
+        }
+        else if (y == 0)
+        {
+            return 1;
+        }
+        return x * _pow_recursion(x, y - 1);
+    }
+
+    static int _sqrt_recursion(int n, int iter = 0)
+    {
+        if (n <= 1 || iter >= n / 2)
+        {
+            return -1;
+        }
+        else if (iter * iter != n)
+        {
+            if ((n % 2) == 1 && iter == 0)
+            {
+                iter++;
+            }
+            return _sqrt_recursion(n, iter + 2);
+        }
+        return iter;
+    }
+}
---
+++ alx-low_level_programming/alx-low_level_programming/0x08-recursion/Program.cs
@@ -0,0 +1,32 @@
+﻿WriteLine("Task 0");
+_puts_recursion("Puts with recursion");
+
+
+WriteLine("\n\nTask 1");
+_print_rev_recursion("\nColton Walker");
+
+WriteLine("\n\nTask 2");
+WriteLine(_strlen_recursion("Corbin Coleman"));
+
+WriteLine("\n\nTask 3");
+WriteLine(factorial(1));
+WriteLine(factorial(5));
+WriteLine(factorial(10));
+WriteLine(factorial(-1024));
+
+WriteLine("\n\nTask 4");
+WriteLine(_pow_recursion(1, 10));
+WriteLine(_pow_recursion(1024, 0));
+WriteLine(_pow_recursion(2, 16));
+WriteLine(_pow_recursion(5, 2));
+WriteLine(_pow_recursion(5, -2));
+WriteLine(_pow_recursion(-5, 3));
+
+
+WriteLine("\n\nTask 5");
+WriteLine(_sqrt_recursion(1));
+WriteLine(_sqrt_recursion(1024));
+WriteLine(_sqrt_recursion(16));
+WriteLine(_sqrt_recursion(17));
+WriteLine(_sqrt_recursion(25));
+WriteLine(_sqrt_recursion(-1));
